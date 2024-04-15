af7c4642a24c020dad0375f0779b153807b08ded
bhswilliaml <bhswilliamli@gmail.com>
2024-04-15 13:45:05 -0700

    Powerup Class
    Created simple powerupclass with a speedup template that allows the player to go faster or slower - should be easy to make other templates to access other variables in movement and change them

--- Sidescroller Test/Assets/Scripts/Powerup Scripts/SpeedUp.cs
+++ Sidescroller Test/Assets/Scripts/Powerup Scripts/SpeedUp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [CreateAssetMenu(menuName = "Powerups/Speedup")]
 public class SpeedUp : PowerupEffect
 {
@@ -8,7 +9,7 @@ public class SpeedUp : PowerupEffect
 
     public override void Apply(GameObject target)
     {
-        target.GetComponent<Movement>().moveSpeed.value += amount;
+        target.GetComponent<Movement>().moveSpeed += amount;
     }
 
 }
