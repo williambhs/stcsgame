b674203669524b34764956b1d5c011fe6db40eb8
1Cyr <s-youngcy@bsd405.org>
2024-04-20 17:38:28 -0700

    Explosion working

--- Sidescroller Test/Assets/Scripts/Explosion.cs
+++ Sidescroller Test/Assets/Scripts/Explosion.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Explosion : MonoBehaviour
 {
@@ -10,14 +12,19 @@ public class Explosion : MonoBehaviour
     //[SerializeField] private GameObject player;
     [SerializeField] private float explosionPower = 30;
     [SerializeField] private float explosionRadius = 10;
+    [SerializeField] private float maxDistance;
     private float timeRemaining = 4;
     private bool timerIsRunning = false;
+    private Text countdownLabel;
 
     // Start is called before the first frame update
     void Start()
     {
         timerIsRunning = true;
         GetComponent<SpriteRenderer>().color = Color.green;
+        countdownLabel = FindObjectOfType<Text>();
+
+        UpdateCountdownLabel(timeRemaining);
     }
 
     // Update is called once per frame
@@ -29,9 +36,13 @@ public class Explosion : MonoBehaviour
             if (timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
+
+                UpdateCountdownLabel(timeRemaining);
             }
             else
             {
+                UpdateCountdownLabel(0);
+
                 timeRemaining = 0;
                 timerIsRunning = false;
 
@@ -42,25 +53,77 @@ public class Explosion : MonoBehaviour
         }
     }
 
+    private void UpdateCountdownLabel(float seconds)
+    {
+        countdownLabel.text = seconds.ToString("0.00") + "s";
+    }
+
 
     // Return a value between 0 and explostion power, based on the other object's position
     // reltative to this bomb
-    public float GetRelativePower(Vector2 otherObjectPosition)
+    public Vector2 GetRelativePower(Vector2 otherObjectPosition)
     {
-        float distance = Vector2.Distance(transform.position, otherObjectPosition);
-
-        distance = Mathf.Abs(distance);
-
+        //float distance = Vector2.Distance(transform.position, otherObjectPosition);
+        // distance = Mathf.Abs(distance);
         // Make sure the distance isn't larger than the blast radius. Otherwise, the
         // calculations will be wrong.
-        distance = Mathf.Min(distance, explosionRadius);
+        //distance = Mathf.Min(distance, explosionRadius);
 
+
+        //calculate x and y distance 
+        //float distanceX = otherObjectPosition.x - transform.position.x;
+        //float distanceY = otherObjectPosition.y - transform.position.y;
         // The further away the other object is, the smaller the value will be. A distance of 0
         // will get the full explosion power. A distance >= the explosion radius will get none.
-        float relativePower = explosionPower * (1 - distance / explosionRadius);
-        
+
+        float relativePowerX = GetRelativePowerForAxis(transform.position.x, otherObjectPosition.x);
+        float relativePowerY = GetRelativePowerForAxis(transform.position.y, otherObjectPosition.y);
+
+        //if (distanceX < maxDistanceX && distanceX > minDistanceX)
+        //{
+        //    relativePowerX = Mathf.Sign(distanceX) * explosionPower * (1 - Mathf.Abs(distanceX) / explosionRadius);
+        //    relativePowerX = 3* explosionPower / distanceX;//* (1 - distanceX / explosionRadius);
+        //}
+        //if (distanceY < maxDistanceY && distanceY > minDistanceY)
+        //{
+        //    relativePowerY = (explosionPower / 3) / (distanceY + 1);//* (1 - distanceY / explosionRadius);
+        //}
+
+        return new Vector2(relativePowerX, relativePowerY);
+    }
+
+    private float GetRelativePowerForAxis(float explositonPosition, float otherObjectPosition)
+    {
+        float distance = otherObjectPosition - explositonPosition;
+
+        float absDistance = Mathf.Abs(distance);
+        float relativePower = 0;
+
+        if (absDistance < maxDistance && absDistance > 0.1f)
+        {
+            relativePower = Mathf.Sign(distance) * explosionPower * (1 - absDistance / explosionRadius);
+        }
+
         return relativePower;
     }
 
-    
+
+    // Return a value between 0 and explostion power, based on the other object's position
+    // reltative to this bomb
+    public Vector2 GetAppliedForce(Vector2 otherObjectPosition)
+    {
+        Vector2 direction = otherObjectPosition - new Vector2(transform.position.x, transform.position.y);
+
+
+        float distance = direction.magnitude;
+
+        direction.Normalize();
+
+        float falloff = 1 - (distance / explosionRadius);
+
+        float force = falloff * explosionPower;
+
+        return (force * direction);
+    }
+
 }
--- Sidescroller Test/Assets/Scripts/ReworkedMovement.cs
+++ Sidescroller Test/Assets/Scripts/ReworkedMovement.cs
@@ -14,16 +14,32 @@ public class ReworkedMovement : MonoBehaviour
     private bool grounded;
     private bool bombIsActive = false;
     private GameObject bombInstance;
+    private bool slideReady;
+    private float currentSpeed;
+    private float hinderanceTimeRemaining = 0;
+    private bool hinderance;
+    //checks how much longer the player should be sliding for. This is for animation
+    private float slideTimeRemaining = 0;
+    //checks how much longer until player can slide again
+    private float slideReadyTimeRemaining = 0;
+    private bool isSliding;
+    [SerializeField] private float slidePower;
 
     private void Awake()
     {
+        //these grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        isSliding = false;
+        slideReady = true;
+        isSliding = false;
+        hinderance = false;
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        //these grab references for rigidbody and animator from object
+       
        
     }
 
@@ -31,7 +47,18 @@ public class ReworkedMovement : MonoBehaviour
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        /*if (horizontalInput != 0)
+        {
+            if (hinderance == true)
+            {
+                body.velocity = new Vector2(horizontalInput * speed - 4, body.velocity.y);
+            }
+            else
+            {
+                body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+            } 
+        }*/
         
         //flip player when moving in respective direction
         if (horizontalInput > 0.01f)
@@ -47,12 +74,69 @@ public class ReworkedMovement : MonoBehaviour
         //checking to see if space is pressed so player can jump
        if (Input.GetKey(KeyCode.Space) && grounded)
         {
+            //jumping will reset slide cooldown instantly
+            isSliding = false;
+            slideReady = true;
             Jump();
         }
 
         //Set animator parameters
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", grounded);
+
+        //checking to see if c is pressed so player can slide;
+        if (Input.GetKey(KeyCode.C) && slideReady == true)
+        {
+            slideReady = false;
+            isSliding = true;
+            Slide();
+        }
+        
+        //checking if the player is sliding for animation 
+        if (isSliding == true)
+        {
+            if (slideTimeRemaining > 0)
+            {
+                slideTimeRemaining -= Time.deltaTime;
+            }
+
+            else
+            {
+                slideTimeRemaining = 0;
+                isSliding = false;
+                slideReady = false;
+                slideReadyTimeRemaining = 2;
+                anim.SetBool("sliding", false);
+            }
+        }
+        //checking if player can slide again
+        if (slideReady == false)
+        {
+            if (slideReadyTimeRemaining > 0)
+            {
+                slideReadyTimeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                slideReadyTimeRemaining = 0;
+                slideReady = true;
+      
+            }
+        }
+        //checking if hinderance from jumping should still be applied to speed
+        /*if (hinderance == true)
+        {
+            if (hinderanceTimeRemaining > 0)
+            {
+                hinderanceTimeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                hinderanceTimeRemaining = 0;
+                hinderance = false;
+
+            }
+        }*/
     }
 
    private void Jump()
@@ -60,6 +144,8 @@ public class ReworkedMovement : MonoBehaviour
         body.velocity = new Vector2(body.velocity.x, jumpPower);
         anim.SetTrigger("jump");
         grounded = false;
+        /*hinderance = true;
+        hinderanceTimeRemaining = 1.25f;*/
 
         if (!bombIsActive)
         {
@@ -75,10 +161,33 @@ public class ReworkedMovement : MonoBehaviour
 
             var explosion = bombInstance.GetComponent<Explosion>();
 
+            bombInstance.transform.position = transform.position;
+
             explosion.Exploded += OnBombExploded;
 
             bombIsActive = true;
         }
+    }
+
+    private void Slide()
+    {
+        /*hinderance = false;
+        hinderanceTimeRemaining = 0;*/
+        anim.SetBool("sliding", true);
+        if (slidePower == 1)
+        {
+            Vector2 slideForce = new Vector2(10, 0);
+            body.AddForce(slideForce);
+                
+        }
+
+        else
+        {
+            body.velocity += new Vector2(body.velocity.x * 8, 0);
+        }
+
+        isSliding = true;
+        slideTimeRemaining = 0.5f;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -97,15 +206,20 @@ public class ReworkedMovement : MonoBehaviour
 
         // Do something with this event.
 
-        float relativePower = explosion.GetRelativePower(transform.position);
+        //Vector2 powers = explosion.GetRelativePower(transform.position);
 
-        // There's extra math needed here for the x and y axes.
-        body.velocity += new Vector2(relativePower, relativePower);
+        //// There's extra math needed here for the x and y axes.
+        ////body.velocity += new Vector2(relativePower, relativePower);
+        //body.velocity += powers;
 
+        Vector2 force = explosion.GetAppliedForce(transform.position);
+
+        body.AddForce(force, ForceMode2D.Impulse);
+        
         bombIsActive = false;
     }
 
-
+    
     //private void getDistance()
     //{
     //    distanceX = player.transform.position.x - transform.position.x;
