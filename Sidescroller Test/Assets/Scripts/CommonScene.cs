286d91a7077210bfb10e1857a4e89ba05cb8779d
1Cyr <s-youngcy@bsd405.org>
2024-05-01 21:20:04 -0700

    added timer and collision

--- Sidescroller Test/Assets/Scripts/CommonScene.cs
+++ Sidescroller Test/Assets/Scripts/CommonScene.cs
@@ -22,7 +22,7 @@ public class CommonScene : MonoBehaviour, IGameObjectOwner
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.E))
         {
             Debug_AddHighScore();
         }
